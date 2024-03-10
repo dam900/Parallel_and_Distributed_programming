b0597529e6af393cabb3d3ac3eac1d79d424cb03
dam900 <d.jakubowski@xberry.tech>
2024-03-10 21:02:42 +0100

    changed the program to use MPI_Reduce

--- euler_gamma_const.cpp
+++ euler_gamma_const.cpp
@@ -34,34 +34,22 @@ void run_client_task(int my_rank, int num_procs) {
 
     if (DEBUG) std::cout << "Hello from client task: " << my_rank << std::endl;
 
-    double buffer_in[num_procs-1];
-    MPI_Request request_buffer[num_procs-1];
-    MPI_Status statuses_buffer[num_procs-1];
+    double sum;
+    double sendbuf;
+    MPI_Reduce(&sendbuf, &sum, 1, MPI_DOUBLE, MPI_SUM, num_procs-1, MPI_COMM_WORLD);
 
-    for (int i = 0; i < num_procs-1; i++){
-        MPI_Irecv(&buffer_in[i], 1, MPI_DOUBLE, i, PART_OF_SUM, MPI_COMM_WORLD, &request_buffer[i]);
-    }
-    
-    int flag;
-    do {
-        MPI_Testall(num_procs-1, request_buffer, &flag, statuses_buffer);
-    } while (!flag);
-
-    double sum = 0;
-    for (int i = 0; i < num_procs-1; i++){
-        sum += buffer_in[i];
-    }
     std::cout << "sum is equal to " << sum << std::endl;
 
 }
 
 void run_worker_task(int my_rank, int num_procs, int p) {
     if (DEBUG) std::cout << "Hello from worker task: " << my_rank << std::endl;
+    double recvbuf;
     double sum = 0;
     double log_part = log((double)((num_procs-1)*p));
+
     for (int i = my_rank*p + 1; i < (my_rank+1)*p; i++){
         sum += (1 / i ) - log_part;
     }
-
-    MPI_Send(&sum, 1, MPI_DOUBLE, num_procs-1, PART_OF_SUM, MPI_COMM_WORLD);
+    MPI_Reduce(&sum, &recvbuf, 1, MPI_DOUBLE, MPI_SUM, num_procs-1, MPI_COMM_WORLD);
 }
