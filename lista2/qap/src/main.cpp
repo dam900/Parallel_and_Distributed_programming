7cfef8e98481b6e5ae6cfd4eac9a7652b106fde4
dam900 <d.jakubowski@xberry.tech>
2024-05-03 15:19:04 +0200

    Broadcasted flow and dist matrix

--- lista2/qap/src/main.cpp
+++ lista2/qap/src/main.cpp
@@ -11,27 +11,29 @@ int main(int argc, char** argv) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0) {
-        std::string filename = "/home/dam900/Downloads/qapdata/chr20a.dat";
-        QapDataReader reader = QapDataReader();
-        auto [n, flowMatrix, distanceMatrix] = reader.fromDataFile(filename);
-
-        std::cout << "n: " << n << std::endl;
-        std::cout << "Flow Matrix: " << std::endl;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                std::cout << flowMatrix[i][j] << " ";
-            }
-            std::cout << std::endl;
+    if (argc < 2) {
+        if (rank == 0) {
+            std::cout << "Usage: " << argv[0] << " <n> <filename>" << std::endl;
         }
-        std::cout << "Distance Matrix: " << std::endl;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                std::cout << distanceMatrix[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
+
+    int n = std::stoi(argv[1]);
+    std::string filename = std::string(argv[2]);
+    IntMatrix flowMatrix(n, std::vector<int>(n));
+    IntMatrix distanceMatrix(n, std::vector<int>(n));
+
+    if (rank == 0) {
+        QapDataReader reader = QapDataReader();
+        auto [n, f, d] = reader.fromDataFile(filename);
+        flowMatrix = f;
+        distanceMatrix = d;
+    }
+    for (int i = 0; i < n; i++) {
+        MPI_Bcast(flowMatrix[i].data(), n, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(distanceMatrix[i].data(), n, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
     MPI_Finalize();
     return 0;
 }
